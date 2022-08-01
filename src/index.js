0768d2530ad9a74033f12b84e08ec413b6e08f81
Lazyuno1987 <95475727+Lazyuno1987@users.noreply.github.com>
2022-08-01 21:45:51 +0300

    start

---
+++ src/index.js
@@ -0,0 +1,89 @@
+const minuteEl = document.querySelector('.minute')
+const secondEl = document.querySelector('.seconds')
+const milisecondEl = document.querySelector('.miliseconds')
+
+
+const startBtn = document.querySelector('.start')
+const stopBtn = document.querySelector('.stop')
+const waitBtn = document.querySelector('.wait')
+const resetBtn = document.querySelector('.reset')
+
+startBtn.addEventListener('click', start)
+// waitBtn.addEventListener('ondblclick', wait)
+waitBtn.ondblclick = wait
+stopBtn.addEventListener('click', stop)
+resetBtn.addEventListener('click', reset)
+
+let minute = 00;
+let seconds = 00;
+let miliseconds = 00;
+let interval;
+
+
+function start() {
+    clearInterval(interval)
+    interval = setInterval(startTimer, 10 )
+}
+
+function stop() {
+    clearInterval(interval)
+    miliseconds = 0
+    seconds = 0
+    minute = 0
+    secondEl.textContent = "00"
+    minuteEl.textContent ='00'
+    
+}
+
+
+function reset() {
+    clearInterval(interval)
+    const results = document.querySelector('.results')
+    const block = document.createElement('div')
+    block.innerText = `Result: ${minute}:${seconds}`
+    results.append(block)
+    miliseconds = 0
+    seconds = 0
+    minute = 0
+    secondEl.textContent = "00"
+    minuteEl.textContent = '00'
+    start()
+    
+
+}
+function wait() {
+    clearInterval(interval)
+}
+
+function startTimer() {
+    miliseconds += 1;
+    if (miliseconds < 9) {
+        milisecondEl.innerText = '0' + miliseconds
+    }
+    if (miliseconds > 9) {
+        milisecondEl.innerText =  miliseconds 
+    }
+  
+    if (miliseconds > 99) {
+        seconds += 1;
+        secondEl.innerText = "0" + seconds
+        miliseconds = 0
+        miliseconds.innerText = '0' + miliseconds
+        
+    }
+if (seconds < 9) {
+        secondEl.innerText = '0' + seconds
+    }
+    if (seconds > 9) {
+        secondEl.innerText =  seconds 
+    }
+  
+    if (seconds > 59) {
+        minute += 1;
+        minuteEl.innerText = "0" + minute
+        seconds = 0
+        seconds.innerText = '0' + seconds
+        
+    }
+    
+}
